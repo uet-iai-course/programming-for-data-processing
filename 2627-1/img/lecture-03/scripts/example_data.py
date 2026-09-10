12b65ab5e5b19a9039407214d1d0dbbac84e8932
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2026-09-10 19:58:43 +0700

    Publish revised lesson 3 NumPy materials

---
+++ 2627-1/img/lecture-03/scripts/example_data.py
@@ -0,0 +1,9 @@
+"""Ví dụ chung của Bài 3. Các mảng số nhỏ để minh hoạ."""
+import numpy as np
+
+A = np.array([[10, 12, 11],
+              [20, 21, 24],
+              [30, 33, 31],
+              [40, 44, 42]], dtype=np.int64)
+B = np.array([1, 2, 3], dtype=np.int64)
+ROW_COLORS = ['#1E93AB', '#E8890C', '#2E8B57', '#8b6bb1']
